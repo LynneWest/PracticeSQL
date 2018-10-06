776c146c657a57c085eb5164c5d447946f4b8ca5
Lynne West <thislynnewest@gmail.com>
2018-10-06 12:38:48 -0700

    Calculate OrderTotal column values using joins with aggregate functions

--- StoreDB_aggragate_with_joins.sql
+++ StoreDB_aggragate_with_joins.sql
@@ -5,5 +5,5 @@ FROM [ORDER]
 	INNER JOIN (	SELECT ORDER_LINE_ITEM.OrderID, SUM(UnitCost * Quantity) AS temp
 					FROM ORDER_LINE_ITEM INNER JOIN ITEM
 						ON ITEM.ItemNumber = ORDER_LINE_ITEM.ItemNumber
-						GROUP BY ORDER_LINE_ITEM.OrderID) AS total
+					GROUP BY ORDER_LINE_ITEM.OrderID) AS total
 		ON [ORDER].OrderID = total.OrderID;
